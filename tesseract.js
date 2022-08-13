b809bed918dcfec1bb58251d06f886fd82edabea
David Walker <d0sboots@gmail.com>
2022-08-13 02:50:24 -0700

    addPolygon

--- tesseract.js
+++ tesseract.js
@@ -44,6 +44,38 @@ function initShaderProgram(vsSource, fsSource, attribs, uniforms) {
   };
 }
 
+function addPolygon(buffers, bufferIdx, color, points) {
+  color = color | 0;
+  const v0x = points[3] - points[0];
+  const v0y = points[4] - points[1];
+  const v0z = points[5] - points[2];
+  const v1x = points[6] - points[0];
+  const v1y = points[7] - points[1];
+  const v1z = points[8] - points[2];
+  const cross = [
+    v0y * v1z - v1y * v0z,
+    v0z * v1x - v1z * v0x,
+    v0x * v1y - v1x * v0y];
+  const max_ind = Math.abs(cross[0]) > Math.abs(cross[1]) ?
+    (Math.abs(cross[0]) > Math.abs(cross[2]) ? 0 : 2) :
+    (Math.abs(cross[1]) > Math.abs(cross[2]) ? 1 : 2);
+  const scale = 1.0 / Math.abs(cross[max_ind]);
+  const nx = Math.round((cross[0] * scale + 1.0) * 127.5);
+  const ny = Math.round((cross[1] * scale + 1.0) * 127.5);
+  const nz = Math.round((cross[2] * scale + 1.0) * 127.5);
+  const normal = nx | (ny << 8) | (nz << 16);
+  const norm_color = [normal, color];
+
+  const floatBuffer = new Float32Array(buffers.vertexArray);
+  const uint32Buffer = new Uint32Array(buffers.vertexArray);
+
+  for (var i = 0; i < points.length; i++) {
+    floatBuffer.set(points.slice(i*3, i*3 + 3), (i + bufferIdx) * 5);
+    uint32Buffer.set(norm_color, (i + bufferIdx) * 5 + 3);
+  }
+  buffers.vertexCount += (points.length / 3) | 0;
+}
+
 function initBuffers(squareAttribs) {
   const vao = vao_ext.createVertexArrayOES();
   vao_ext.bindVertexArrayOES(vao);
@@ -53,44 +85,39 @@ function initBuffers(squareAttribs) {
 
   // TRIANGLE_STRIP data, vec3 vertex + (i8) vec3 normal + pad, (u8) vec3 color + pad
   const vertexData = [
-     1.05, -1.05, 0.0, 0x00, 0x7F, 0x7F, 0, 0xFF, 0x0, 0x0, 0x0,
-     1.05,  1.05, 0.0, 0x00, 0x7F, 0x7F, 0, 0xFF, 0x0, 0x0, 0x0,
-    -1.05, -1.05, 0.0, 0x00, 0x7F, 0x7F, 0, 0xFF, 0x0, 0x0, 0x0,
-    -1.05,  1.05, 0.0, 0x00, 0x7F, 0x7F, 0, 0xFF, 0x0, 0x0, 0x0,
+     1.05, -1.05, 0.0,
+     1.05,  1.05, 0.0,
+    -1.05, -1.05, 0.0,
+    -1.05,  1.05, 0.0,
   ];
-
   const stride = 20;
-  const elements = 4;
-  const vertexArray = new ArrayBuffer(stride * elements);
+  const vertexArray = new ArrayBuffer(stride * 16);
+
+  const buffers = {
+    vao: vao,
+    vbo: vertexBuffer,
+    vertexData: vertexData,
+    vertexArray: vertexArray,
+    vertexCount: 0,
+  };
+
+  addPolygon(buffers, 0, 0x000000FF, vertexData);
 
   var numComponents = 3;
   var type = gl.FLOAT;
   var normalize = false;
   var offset = 0;
-  const floatBuffer = new Float32Array(vertexArray)
   gl.vertexAttribPointer(squareAttribs.position, numComponents, type, normalize, stride, offset);
-  for (var i = 0; i < elements; i++) {
-    floatBuffer.set(vertexData.slice(i*11, i*11 + 3), i * 5)
-  }
 
   numComponents = 4;
   type = gl.BYTE;
   normalize = true;
   offset = 12;
-  const int8Buffer = new Int8Array(vertexArray)
   gl.vertexAttribPointer(squareAttribs.normal, numComponents, type, normalize, stride, offset);
-  for (var i = 0; i < elements; i++) {
-    int8Buffer.set(vertexData.slice(i*11 + 3, i*11 + 7), i * 20 + 12)
-  }
 
   type = gl.UNSIGNED_BYTE;
-  normalize = true;
   offset = 16;
-  const uint8Buffer = new Uint8Array(vertexArray)
   gl.vertexAttribPointer(squareAttribs.color, numComponents, type, normalize, stride, offset);
-  for (var i = 0; i < elements; i++) {
-    uint8Buffer.set(vertexData.slice(i*11 + 7, i*11 + 11), i * 20 + 16)
-  }
 
   gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
@@ -98,14 +125,7 @@ function initBuffers(squareAttribs) {
   gl.enableVertexAttribArray(squareAttribs.normal);
   gl.enableVertexAttribArray(squareAttribs.color);
   vao_ext.bindVertexArrayOES(null);
-
-  return {
-    vao: vao,
-    vbo: vertexBuffer,
-    vertexData: vertexData,
-    vertexArray: vertexArray,
-    vertexCount: elements,
-  };
+  return buffers;
 }
 
 function initGLState() {
